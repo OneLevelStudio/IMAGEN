1ab8f082ba82a4a925bdc208f68eff1b75e1c471
baobuiquang <quangbao.dev@gmail.com>
2025-09-06 19:32:29 +0700

    docker

--- docker/forge/download_models.sh
+++ docker/forge/download_models.sh
@@ -1,11 +1,11 @@
 echo "📥📥📥📥📥"
 
+wget -P FORGE/models/Stable-diffusion https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/CHECKPOINT/WAI_V11.safetensors
 # wget -P FORGE/models/Stable-diffusion https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/CHECKPOINT/LTF_V05.safetensors
-# wget -P FORGE/models/Stable-diffusion https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/CHECKPOINT/WAI_V11.safetensors
-wget -P FORGE/models/Stable-diffusion https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/CHECKPOINT/MJX_V07.safetensors
-wget -P FORGE/models/Lora             https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/LORA/LORA_KoreaDL.safetensors
-wget -P FORGE/models/Lora             https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/LORA/LORA_JapanDL.safetensors
-wget -P FORGE/models/Lora             https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/LORA/LORA_TaiwnDL.safetensors
-wget -P FORGE/models/Lora             https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/LORA/LORA_ChinaDL.safetensors
+# wget -P FORGE/models/Stable-diffusion https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/CHECKPOINT/MJX_V07.safetensors
+# wget -P FORGE/models/Lora             https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/LORA/LORA_KoreaDL.safetensors
+# wget -P FORGE/models/Lora             https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/LORA/LORA_JapanDL.safetensors
+# wget -P FORGE/models/Lora             https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/LORA/LORA_TaiwnDL.safetensors
+# wget -P FORGE/models/Lora             https://huggingface.co/onelevelstudio/IMAGEN/resolve/main/LORA/LORA_ChinaDL.safetensors
 
 echo "✅✅✅✅✅"
