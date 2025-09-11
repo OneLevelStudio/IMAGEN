7301c149c4ed7a0060fe72d5ffaaaff0f6f46ac5
baobuiquang <quangbao.dev@gmail.com>
2025-09-11 11:33:05 +0700

    update

--- docker/comfy/download_models.sh
+++ docker/comfy/download_models.sh
@@ -1,4 +1,4 @@
-echo "📥📥📥📥📥"
+echo "📥 =================================================="
 
 wget -P COMFY/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors
 wget -P COMFY/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors
@@ -7,4 +7,4 @@ wget -P COMFY/models/loras            https://huggingface.co/Comfy-Org/Wan_2.2_C
 wget -P COMFY/models/loras            https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_low_noise.safetensors
 wget -P COMFY/models/vae              https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
 
-echo "✅✅✅✅✅"
+echo "✅ =================================================="
